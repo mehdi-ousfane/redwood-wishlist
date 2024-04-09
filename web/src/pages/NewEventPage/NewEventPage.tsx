3dec7c6aa6c12bba4ce678a94cea2fccfdc99c18
Mehdi Ousfane <ousfane.mehdi@gmail.com>
2024-04-09 23:12:07 +0200

    feature(pages): add new event page logic

--- web/src/pages/NewEventPage/NewEventPage.tsx
+++ web/src/pages/NewEventPage/NewEventPage.tsx
@@ -1,19 +1,80 @@
-import { Link, routes } from '@redwoodjs/router'
-import { MetaTags } from '@redwoodjs/web'
+import { DateField, Form, Label, TextField } from '@redwoodjs/forms'
+import { navigate, routes } from '@redwoodjs/router'
+import { MetaTags, useMutation } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/dist/toast'
+
+import Checkbox from 'src/components/Checkbox/Checkbox'
+import HeaderWithRulers from 'src/components/HeaderWithRulers/HeaderWithRulers'
+
+const CREATE_EVENT_MUTATION = gql`
+  mutation createEventMutation(
+    $name: String!
+    $date: DateTime!
+    $sendReminder: Boolean!
+  ) {
+    createEvent(
+      input: { name: $name, sendReminder: $sendReminder, date: $date }
+    ) {
+      id
+      name
+      date
+      createdAt
+    }
+  }
+`
 
 const NewEventPage = () => {
+  const [createEvent, { loading }] = useMutation(CREATE_EVENT_MUTATION, {
+    onCompleted: () => {
+      toast.success('Event was successfully created.')
+      navigate(routes.groupInvite())
+    },
+    onError: (error) => {
+      console.error({ error })
+      toast.error(error.message)
+    },
+  })
+
+  const handleSubmit = (data) => {
+    console.log({ data })
+    createEvent({
+      variables: {
+        name: data.eventName,
+        date: data.eventDate,
+        sendReminder: data.eventReminder,
+      },
+    })
+  }
+
   return (
     <>
-      <MetaTags title="NewEvent" description="NewEvent page" />
+      <MetaTags title="Set up your Event" />
 
-      <h1>NewEventPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/NewEventPage/NewEventPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>newEvent</code>, link to me with `
-        <Link to={routes.newEvent()}>NewEvent</Link>`
-      </p>
+      <div className="auth-form">
+        <HeaderWithRulers
+          className="mb-8 text-white"
+          heading="Set up Your Event"
+        />
+        <Form onSubmit={handleSubmit}>
+          <fieldset disabled={loading}>
+            <div className="field">
+              <Label name="eventName">Event Name</Label>
+              <TextField name="eventName" placeholder="" />
+            </div>
+            <div className="field">
+              <Label name="eventDate">Event Date</Label>
+              <DateField name="eventDate" placeholder="" />
+            </div>
+            <div className="field">
+              <Checkbox
+                label="Send out a reminder for an event"
+                name="eventReminder"
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </fieldset>
+        </Form>
+      </div>
     </>
   )
 }
