d29a4c8278f83a2e02cc4dd6eedc4943ba269cb0
Mehdi Ousfane <ousfane.mehdi@gmail.com>
2024-04-16 22:30:31 +0200

    chore(routes): change route organization

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -15,14 +15,14 @@ import InteriorLayout from './layouts/InteriorLayout/InteriorLayout'
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
-      <Set wrap={InteriorLayout}>
-        <Route path="/event/{id:String}" page={GroupInvitePage} name="groupInvite" />
-      </Set>
       <PrivateSet unauthenticated="login">
         <Set wrap={AuthLayout}>
           <Route path="/event/new" page={NewEventPage} name="newEvent" />
         </Set>
-        <Route path="/dashboard" page={DashboardPage} name="dashboard" />
+        <Set wrap={InteriorLayout}>
+          <Route path="/event/{id:String}" page={GroupInvitePage} name="groupInvite" />
+          <Route path="/dashboard" page={DashboardPage} name="dashboard" />
+        </Set>
       </PrivateSet>
       <Set wrap={AuthLayout}>
         <Route path="/login" page={LoginPage} name="login" />
