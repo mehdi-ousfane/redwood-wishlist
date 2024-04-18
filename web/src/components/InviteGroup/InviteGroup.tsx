639663479d3f78b2cf562b996cd98d746ac4990c
Mehdi Ousfane <ousfane.mehdi@gmail.com>
2024-04-18 17:20:25 +0200

    feature(components): change the invite group logic and add mutation

--- web/src/components/InviteGroup/InviteGroup.tsx
+++ web/src/components/InviteGroup/InviteGroup.tsx
@@ -1,52 +1,72 @@
-import Card from '../Card/Card'
+import { EmailField, Form, Label, TextField, useForm } from '@redwoodjs/forms'
+import { useMutation } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/dist/toast'
+
+import ListInvitesCell from '../ListInvitesCell'
 import RoundButton from '../RoundButton/RoundButton'
 
-const InviteGroup = () => {
+const CREATE_INVITE_MUTATION = gql`
+  mutation createInviteMutation(
+    $eventId: String!
+    $email: String!
+    $name: String!
+  ) {
+    createInvite(
+      input: { eventId: $eventId, status: INVITED, email: $email, name: $name }
+    ) {
+      email
+      id
+      name
+      status
+    }
+  }
+`
+
+const InviteGroup = ({ id }) => {
+  const formMethods = useForm()
+
+  const [createInvite, { loading }] = useMutation(CREATE_INVITE_MUTATION, {
+    onError: (error) => {
+      toast.error(error.message)
+      console.error(error.message)
+    },
+    onCompleted: () => {
+      toast.success('Invite sent!')
+      formMethods.reset()
+    },
+  })
+
+  const handleSubmit = (data) => {
+    console.log({ data })
+    createInvite({
+      variables: { ...data, eventId: id },
+    })
+  }
+
   return (
     <div>
       {/* form */}
-      <div className="label ml-5">Invite a friend or family member</div>
-      <div className="mb-10 ml-5 flex items-center gap-5 bg-spanishGreen p-4">
-        <div className="field flex-1">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            className="input"
-            placeholder=""
-          />
-        </div>
-        <div className="field flex-1">
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            className="input"
-            placeholder=""
-          />
-        </div>
-        <RoundButton status="warning" />
+      <div className="label ml-5 text-black dark:text-white">
+        Invite a friend or family member
       </div>
+      <Form onSubmit={handleSubmit} formMethods={formMethods}>
+        <fieldset disabled={loading}>
+          <div className="mb-10 ml-5 flex items-center gap-5 bg-spanishGreen p-4 dark:bg-blackPearl">
+            <div className="field mb-0 flex-1">
+              <Label name="name">Name</Label>
+              <TextField name="name" className="input" placeholder="" />
+            </div>
+            <div className="field mb-0 flex-1">
+              <Label name="email">Email</Label>
+              <EmailField name="email" className="input" placeholder="" />
+            </div>
+            <RoundButton status="warning" type="submit" />
+          </div>
+        </fieldset>
+      </Form>
 
       <div className="grid grid-cols-2 gap-x-12 gap-y-8">
-        <Card
-          avatar={{
-            alt: 'Avatar',
-            avatar: 'https://picsum.photos/seed/1701322447715/300/300',
-          }}
-          email="email@email.com"
-          name="Amy Dutton"
-        />
-        <Card
-          avatar={{
-            alt: 'Avatar',
-            avatar: 'https://picsum.photos/seed/1701322447715/300/300',
-          }}
-          email="email@email.com"
-          name="Amy Dutton"
-        />
+        <ListInvitesCell id={id} />
       </div>
     </div>
   )
