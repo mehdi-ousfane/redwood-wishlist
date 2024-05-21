d171af6e969fed552e7d6adee96860a3efb179f5
Mehdi Ousfane <ousfane.mehdi@gmail.com>
2024-05-21 15:26:22 +0200

    feature(components): generate a new comp with rw

---
+++ web/src/components/EventForm/EventForm.tsx
@@ -0,0 +1,10 @@
+const EventForm = () => {
+  return (
+    <div>
+      <h2>{'EventForm'}</h2>
+      <p>{'Find me in ./web/src/components/EventForm/EventForm.tsx'}</p>
+    </div>
+  )
+}
+
+export default EventForm
